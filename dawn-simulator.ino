6304e080cca13c4cf5b70ac0ade8b3b58d8a5210
Mickael <fontaine.mickael@ymail.com>
2016-01-31 01:00:19 +0100

    Update dawn-simulator.ino

--- dawn-simulator.ino
+++ dawn-simulator.ino
@@ -1,42 +1,71 @@
 int red = 9;           
 int green = 10;
 int blue = 11;
+int outsideBrightness = A5;
+int lampSwitch = 2;
+int offLed = 7;
+
 int blueSat = 0;
 int redSat = 0;
 int greenSat = 0;
-int potentInput = A5;
-int potentVoltage = 2;
+
 bool tokenStop=0;
 int storedBrightness;
-int brightness = 250;    // how bright outside is
-int fadeAmount = 5;    // how many points to fade the LED by
-
+int mode = 1; //0 = eteint;1 = Veilleuse,lumino
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
   pinMode(red, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(green, OUTPUT);
+  pinMode(lampSwitch,INPUT);
+  pinMode(offLed,OUTPUT);
   Serial.begin(9600);
-            rgb(255,255,255);
-         delay(500);
+           
          //test Rouge
-        rgb(255,0,0);
-          delay(500);
+        rgb(30,0,0);
+          delay(250);
          //test green
-          rgb(0,255,0);
-         delay(500);
+          rgb(0,30,0);
+         delay(250);
         //test blue
-         rgb(0,0,255);
-         delay(500);
+         rgb(0,0,30);
+         delay(250);
          rgb(0,0,0);
+          rgb(30,30,30);
+         delay(250);
+  attachInterrupt(digitalPinToInterrupt(lampSwitch), switchMode, CHANGE);
 }
 
 void loop() {
 
   simulatedDawn();
+  while(mode!=0){
+    rgb(255,255,255);
+  }
+  off();
 
+}
 
+void switchMode(){
+  mode++;
+  if(mode==2){
+      mode=0;
+  }
+  Serial.println("mode");
+  Serial.println(mode);
+  delay(500);
+  
+
+}
+
+void off(){
+  while(mode==0){
+    digitalWrite(offLed,HIGH);
+    rgb(0,0,0);
+  }
+  digitalWrite(offLed,LOW);  
+  
 }
 
 void rgb(int r,int g,int b){
@@ -53,10 +82,17 @@ void rgb(){
     analogWrite(blue, blueSat);
 
 }
+void veilleuse(){
+    rgb(40,20,40);
+}
 
 int upLight(int value,int factor){
+
   
-  brightness=analogRead(potentInput);
+  if(mode==0){
+     return 0;
+  }
+ int brightness=analogRead(outsideBrightness);
   if(storedBrightness<=brightness){
     storedBrightness=brightness;
     tokenStop=0;
@@ -71,17 +107,27 @@ int upLight(int value,int factor){
 
     value=255;
   }
-  delay(100);
+  delay(170);
   return value;
 }
 
-void simulatedDawn(){
+int simulatedDawn(){
   greenSat=0;
   redSat=0;
   blueSat=0;
-  while(greenSat<128){
+  int brightness = analogRead(outsideBrightness);
+  //brightness = 5;//debug
+  while(brightness<10){
+      if(mode==0){
+          return 0;
+      }
+      veilleuse();
+  }
+  while(greenSat<170){
 
-
+          if(mode==0){
+            return 0;
+          }
           greenSat=upLight(greenSat,1);
 
           redSat=upLight(redSat,2);
@@ -90,6 +136,9 @@ void simulatedDawn(){
     }
         
     while(blueSat<202){
+          if(mode==0){
+            return 0;
+          }
           greenSat=upLight(greenSat,1);
 
          blueSat=upLight(blueSat,1);
@@ -98,7 +147,9 @@ void simulatedDawn(){
     }
 
     while(blueSat<255){
-      
+          if(mode==0){
+            return 0;
+          }
          blueSat=upLight(blueSat,1);
          redSat=upLight(redSat,1);
           rgb();
@@ -106,11 +157,16 @@ void simulatedDawn(){
       
     }
     while(redSat<255){
-         
+         if(mode==0){
+            return 0;
+          }
          redSat=upLight(redSat,1);
           rgb();
     }
     while(greenSat<255){
+         if(mode==0){
+            return 0;
+          }
          greenSat=upLight(greenSat,1);
           rgb();
       
@@ -120,3 +176,4 @@ void simulatedDawn(){
 //void jaune(){
 //  rgb(238,245,37);//plus orange => green a 116; plus clair => bleu => 220
 //}
+
