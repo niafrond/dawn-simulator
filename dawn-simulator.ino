c3d77719300c36d8c3384e8b1364fe05dfe998ad
Mickael <fontaine.mickael@ymail.com>
2016-02-04 21:35:45 +0100

    No more modes

--- dawn-simulator.ino
+++ dawn-simulator.ino
@@ -1,24 +1,23 @@
 int red = 9;           
 int green = 10;
 int blue = 11;
+
 int outsideBrightness = A5;
 int lampSwitch = 2;
-int offLed = 7;
 
-int blueSat = 0;
-int redSat = 0;
-int greenSat = 0;
+int blueSat = 0;//BlueSaturation
+int redSat = 0;//RedSaturation
+int greenSat = 0;//GreenSaturation
 
-bool tokenStop=0;
+bool tokenStop=0; //Stop light change
 int storedBrightness;
-int mode = 2; //0 = eteint;1 = Veilleuse,2=Veilleuse+Lumino
+int mode = 2; //0 = eteint;1 = nightLight,2=nightLight+Lumino //Disabled : push button electro magnetic sensible make it unexploitable
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
+  
   pinMode(red, OUTPUT);
   pinMode(blue, OUTPUT);
   pinMode(green, OUTPUT);
-  pinMode(lampSwitch,INPUT);
   pinMode(offLed,OUTPUT);
   Serial.begin(9600);
            
@@ -34,38 +33,22 @@ void setup() {
          rgb(0,0,0);
           rgb(1,1,1);
          delay(250);
-  //attachInterrupt(digitalPinToInterrupt(lampSwitch), switchMode, RISING);
 }
 
 void loop() {
 
-  veilleuse();
+  //While outside light isn(t changing,we put the night light
+  while(readLuminosity()<10){
+     
+      nightLight();
+      
+  }
   simulatedDawn();
   
-  off();
 
 }
 
-void switchMode(){
-  mode++;
-  if(mode==3){
-      mode=0;
-  }
-  Serial.println("mode");
-  Serial.println(mode);
-  
- 
 
-}
-
-void off(){
-  while(mode==0){
-    digitalWrite(offLed,HIGH);
-    rgb(0,0,0);
-  }
-  digitalWrite(offLed,LOW);  
-  
-}
 
 void rgb(int r,int g,int b){
     
@@ -81,68 +64,53 @@ void rgb(){
     analogWrite(blue, blueSat);
 
 }
-void veilleuse(){
-    while(mode==1){
-      rgb(0,3,1);
-      delay(100);
-    }
+void nightLight(){
+
     rgb(0,3,1);
     delay(100);
 }
-
+//We up only if outside if up-ing too
 int upLight(int value,int factor){
 
-  
-  if(mode==0){
-     return 0;
-  }
  int brightness=readLuminosity();
 
   if(storedBrightness<=brightness){
+    //If outside brightness is growing, light inside up
     storedBrightness=brightness;
     tokenStop=0;
   }
   if(value<(brightness * factor) && !tokenStop){
+    //If outside brightness is growing, light inside up
     value=value+factor;
   }else{
-    tokenStop=1;//Plus aucune valeur n'augmente
+    tokenStop=1;//No more light up
   }
   if(value>255){
-
+    //More than 255, make it to 0
     value=255;
   }
-  delay(170);
+  delay(170);//Eye perception
   return value;
 }
 
 int simulatedDawn(){
-  if(mode==2){
-  //On avertit du mode Simulated Dawn
+
+  //brightness = 5;//debug
+  
+   greenSat=0;
+  redSat=0;
+  blueSat=0;
+   //Simulated Dawn Warning
     rgb(0,0,2);
     delay(1000);
     rgb(0,0,0);
     delay(1000);
     rgb(0,0,2);
     delay(1000);
-  }
-  greenSat=0;
-  redSat=0;
-  blueSat=0;
-  
-  //brightness = 5;//debug
-  while(readLuminosity()<10){
-      if(mode!=2){
-           return 0;
-      }
-      veilleuse();
-      
-  }
   rgb(1,1,0);
   while(greenSat<170){
 
-          if(mode!=2){
-            return 0;
-          }
+
           greenSat=upLight(greenSat,1);
 
           redSat=upLight(redSat,2);
@@ -195,13 +163,15 @@ int simulatedDawn(){
 int readLuminosity(){
 
   int light=analogRead(outsideBrightness);
-  light=light/3;
+ 
+  light=light/3; //1024 becomes 255
+  
   Serial.println(light*3);
   Serial.println(light);
   
 
   return light;
 }
-//void jaune(){
-//  rgb(238,245,37);//plus orange => green a 116; plus clair => bleu => 220
-//}
+//yellow tip
+//  rgb(238,245,37);//more orange => green to 116; lighter => blue to 220
+//
